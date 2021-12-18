38f61fc0fc439c835fd955631ddfde272b948f7b
mjan9 <m.jan9396@hotmail.com>
2021-12-18 15:29:41 +0500

    Metra Editor

---
+++ editor_metra/android/app/src/main/kotlin/com/metra/dev/editor_metra/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.metra.dev.editor_metra
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ editor_metra/lib/view/editor_show.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class EditorShow extends StatelessWidget {
+  const EditorShow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Stack(children: const [
+
+
+
+    ],),);
+  }
+}
